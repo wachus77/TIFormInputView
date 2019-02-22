de7dcd9c79cce0d21985e8a9272aaba487fba7e7
Tomasz Iwaszek <tiwaszek@concisesoftware.com>
2019-02-22 15:38:20 +0100

    Initial commit

---
+++ Example/TIFormInputView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TIFormInputView
+//
+//  Created by tomek.iwaszek on 02/22/2019.
+//  Copyright (c) 2019 tomek.iwaszek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
