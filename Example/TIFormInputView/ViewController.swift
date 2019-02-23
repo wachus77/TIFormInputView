c5fd727e94dae152c6bf0265c40f727d6d7c06ae
Tomasz Iwaszek <wachus77@gmail.com>
2019-02-23 11:27:03 +0100

    changed screenshot

--- Example/TIFormInputView/ViewController.swift
+++ Example/TIFormInputView/ViewController.swift
@@ -20,6 +20,26 @@ extension UIViewController {
     }
 }
 
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt32()
+        Scanner(string: hex).scanHexInt32(&int)
+        let a, r, g, b: UInt32
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var email: FormInputView!
@@ -37,6 +57,7 @@ class ViewController: UIViewController {
         email.placeholder = emailPlaceholder
         email.regex = emailRegex
         email.error = emailErrorText
+        email.errorColor = UIColor(hexString: "#a12428")
         
         let addtRulMin8CharRegex = "^.*(?=.{8,}).*$"
         let addtRulUpperLowerCaseRegex = "^.*(?=.*[A-Z])(?=.*[a-z]).*$"
@@ -56,6 +77,7 @@ class ViewController: UIViewController {
         password.placeholder = passwordPlaceholder
         password.regex = passwordRegex
         password.error = passwordErrorText
+        password.errorColor = UIColor(hexString: "#a12428")
         password.initAdditionalValidationRules(additionalRules: additionalRules)
     }
     
